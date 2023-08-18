1072666436b282814e4bf7eaf8d4ae561a057e0f
OliviaYJH <0703olivia@naver.com>
2023-08-18 22:46:33 +0900

    단속카메라

--- JeongHyun/JeongHyunAlgo/src/단속카메라.java
+++ JeongHyun/JeongHyunAlgo/src/단속카메라.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class 단속카메라 {
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 1;
+        Arrays.sort(routes, (a,b) -> Integer.compare(a[1], b[1])); // 끝나는 순으로 오름차순 정렬
+        int location = routes[0][1]; // 처음 위치
+
+        for(int i = 1; i < routes.length; i++){
+            if(routes[i][0] > location){
+                answer++;
+                location = routes[i][1];
+            }
+        }
+
+
+        return answer;
+    }
 }
