455f46878b598088daf88c8864dc0ce561aacac0
jinee <sksowk156@gmail.com>
2023-11-02 20:46:38 +0900

    20231102, 그래프, 그리디

--- Jinhyuk/src/personal/CT1031P.java
+++ Jinhyuk/src/personal/CT1031P.java
@@ -1,215 +1,215 @@
-package personal;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-public class CT1031P {
-    //    public static void main(String[] args) throws IOException {
+//package personal;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.nio.Buffer;
+//import java.util.Arrays;
+//import java.util.StringTokenizer;
+//
+//public class CT1031P {
+//    //    public static void main(String[] args) throws IOException {
+////        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+////        String str = br.readLine();
+////        StringTokenizer st = new StringTokenizer(str, " ");
+////        int x = Integer.parseInt(st.nextToken());
+////        int y = Integer.parseInt(st.nextToken());
+////        int[][] list = new int[x][y];
+////        str = br.readLine();
+////        st = new StringTokenizer(str, " ");
+////        for (int i = 0; i < x; i++) {
+////            for (int j = 0; j < y; j++) {
+////                list[i][j] = Integer.parseInt(st.nextToken());
+////            }
+////        }
+////
+////        Solution temp = new Solution();
+////        System.out.println(temp.solution(list));
+////    }
+////    public static void main(String[] args) throws IOException {
+////        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+////        String str = br.readLine();
+////        int size = Integer.parseInt(str);
+////        int[][] list = new int[size][size];
+////        for (int i = 0; i < size; i++) {
+////            str = br.readLine();
+////            StringTokenizer st = new StringTokenizer(str, " ");
+////            for (int j = 0; j < i + 1; j++) {
+////                list[i][j] = Integer.parseInt(st.nextToken());
+////            }
+////        }
+////
+////        Solution temp = new Solution();
+////        System.out.println(temp.solution(list));
+////    }
+//    public static void main(String[] args) throws IOException {
+////        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+////        String str = br.readLine();
+////        int size = Integer.parseInt(str);
+////        int[][] list = new int[size][2];
+////
+////        for (int i = 0; i < size; i++) {
+////            str = br.readLine();
+////            StringTokenizer st = new StringTokenizer(str, " ");
+////            list[i][0] = Integer.parseInt(st.nextToken());
+////            list[i][1] = Integer.parseInt(st.nextToken());
+////        }
+////        Solution temp = new Solution();
+////        System.out.println(temp.solution(list));
+////        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+////        String str = br.readLine();
+////        int size = Integer.parseInt(str);
+////        int line = Integer.parseInt(br.readLine());
+////        int[][] list = new int[line][3];
+////        for (int i = 0; i < line; i++) {
+////            str = br.readLine();
+////            StringTokenizer st = new StringTokenizer(str, " ");
+////            list[i][0] = Integer.parseInt(st.nextToken());
+////            list[i][1] = Integer.parseInt(st.nextToken());
+////            list[i][2] = Integer.parseInt(st.nextToken());
+////        }
+////        Solution temp = new Solution();
+////        int[][] result = temp.solution(list, size);
+////        for (int i = 0; i < size; i++) {
+////            for (int j = 0; j < size; j++) {
+////                System.out.print(result[i][j] + " ");
+////            }
+////            System.out.println();
+////        }
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        String str = br.readLine();
 //        StringTokenizer st = new StringTokenizer(str, " ");
-//        int x = Integer.parseInt(st.nextToken());
-//        int y = Integer.parseInt(st.nextToken());
-//        int[][] list = new int[x][y];
-//        str = br.readLine();
-//        st = new StringTokenizer(str, " ");
-//        for (int i = 0; i < x; i++) {
-//            for (int j = 0; j < y; j++) {
-//                list[i][j] = Integer.parseInt(st.nextToken());
-//            }
-//        }
+//        int size = Integer.parseInt(st.nextToken());
+//        int count = Integer.parseInt(st.nextToken());
 //
-//        Solution temp = new Solution();
-//        System.out.println(temp.solution(list));
-//    }
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        String str = br.readLine();
-//        int size = Integer.parseInt(str);
-//        int[][] list = new int[size][size];
-//        for (int i = 0; i < size; i++) {
-//            str = br.readLine();
-//            StringTokenizer st = new StringTokenizer(str, " ");
-//            for (int j = 0; j < i + 1; j++) {
-//                list[i][j] = Integer.parseInt(st.nextToken());
-//            }
-//        }
-//
-//        Solution temp = new Solution();
-//        System.out.println(temp.solution(list));
-//    }
-    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        String str = br.readLine();
-//        int size = Integer.parseInt(str);
 //        int[][] list = new int[size][2];
+//        for (int i = 0; i < count; i++) {
+//            str = br.readLine();
+//            st = new StringTokenizer(str, " ");
+//            list[i][0] = Integer.parseInt(st.nextToken());
+//            list[i][1] = Integer.parseInt(st.nextToken());
+//        }
 //
-//        for (int i = 0; i < size; i++) {
-//            str = br.readLine();
-//            StringTokenizer st = new StringTokenizer(str, " ");
-//            list[i][0] = Integer.parseInt(st.nextToken());
-//            list[i][1] = Integer.parseInt(st.nextToken());
-//        }
 //        Solution temp = new Solution();
-//        System.out.println(temp.solution(list));
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        String str = br.readLine();
-//        int size = Integer.parseInt(str);
-//        int line = Integer.parseInt(br.readLine());
-//        int[][] list = new int[line][3];
-//        for (int i = 0; i < line; i++) {
-//            str = br.readLine();
-//            StringTokenizer st = new StringTokenizer(str, " ");
-//            list[i][0] = Integer.parseInt(st.nextToken());
-//            list[i][1] = Integer.parseInt(st.nextToken());
-//            list[i][2] = Integer.parseInt(st.nextToken());
+//        System.out.println(temp.solution(list, size));
+//
+//    }
+//}
+//
+//class Solution {
+//
+//    public int solution(int[][] list, int size) {
+//        int[][] result = new int[size + 1][size + 1];
+//
+//        for (int[] i : list) {
+//            result[i[0]][i[1]] = 1;
 //        }
-//        Solution temp = new Solution();
-//        int[][] result = temp.solution(list, size);
-//        for (int i = 0; i < size; i++) {
-//            for (int j = 0; j < size; j++) {
+//
+//        for (int k = 1; k <= size; k++) {
+//            for (int i = 1; i <= size; i++) {
+//                for (int j = 1; j <= size; j++) {
+//                    if (result[i][k] != 0 && result[k][j] != 0) {
+//                        result[i][j] = 1;
+//                    }
+//                }
+//            }
+//        }
+//
+//        for (int i = 1; i <= size; i++) {
+//            for (int j = 1; j <= size; j++) {
 //                System.out.print(result[i][j] + " ");
 //            }
 //            System.out.println();
 //        }
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str, " ");
-        int size = Integer.parseInt(st.nextToken());
-        int count = Integer.parseInt(st.nextToken());
-
-        int[][] list = new int[size][2];
-        for (int i = 0; i < count; i++) {
-            str = br.readLine();
-            st = new StringTokenizer(str, " ");
-            list[i][0] = Integer.parseInt(st.nextToken());
-            list[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-        Solution temp = new Solution();
-        System.out.println(temp.solution(list, size));
-
-    }
-}
-
-class Solution {
-
-    public int solution(int[][] list, int size) {
-        int[][] result = new int[size + 1][size + 1];
-
-        for (int[] i : list) {
-            result[i[0]][i[1]] = 1;
-        }
-
-        for (int k = 1; k <= size; k++) {
-            for (int i = 1; i <= size; i++) {
-                for (int j = 1; j <= size; j++) {
-                    if (result[i][k] != 0 && result[k][j] != 0) {
-                        result[i][j] = 1;
-                    }
-                }
-            }
-        }
-
-        for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size; j++) {
-                System.out.print(result[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        int count = 0;
-        for (int i = 1; i <= size; i++) {
-            int num = 0;
-            for (int j = 1; j <= size; j++) {
-                num += result[j][i];
-                num += result[i][j];
-            }
-            if (num == size - 1) count++;
-        }
-        return count;
-    }
-
-//    public int[][] solution(int[][] list, int size) {
-//        int[][] result = new int[size][size];
-//        for (int i = 0; i < size; i++) {
-//            for (int j = 0; j < size; j++) {
-//                if (i == j) result[i][j] = 0;
-//                else result[i][j] = 100000;
-//            }
-//        }
 //
-//        for (int[] i : list) {
-//            result[i[0] - 1][i[1] - 1] = Math.min(i[2], result[i[0] - 1][i[1] - 1]);
-//        }
-//
-//        for (int k = 0; k < size; k++) {
-//            for (int i = 0; i < size; i++) {
-//                for (int j = 0; j < size; j++) {
-//                    result[i][j] = Math.min(result[i][j], result[i][k] + result[k][j]);
-//                }
+//        int count = 0;
+//        for (int i = 1; i <= size; i++) {
+//            int num = 0;
+//            for (int j = 1; j <= size; j++) {
+//                num += result[j][i];
+//                num += result[i][j];
 //            }
+//            if (num == size - 1) count++;
 //        }
-//        return result;
+//        return count;
 //    }
-//    public int solution(int[][] list) {
-//        int[] result = new int[list.length + 1];
 //
-//        for (int i = 0; i < list.length; i++) {
+////    public int[][] solution(int[][] list, int size) {
+////        int[][] result = new int[size][size];
+////        for (int i = 0; i < size; i++) {
+////            for (int j = 0; j < size; j++) {
+////                if (i == j) result[i][j] = 0;
+////                else result[i][j] = 100000;
+////            }
+////        }
+////
+////        for (int[] i : list) {
+////            result[i[0] - 1][i[1] - 1] = Math.min(i[2], result[i[0] - 1][i[1] - 1]);
+////        }
+////
+////        for (int k = 0; k < size; k++) {
+////            for (int i = 0; i < size; i++) {
+////                for (int j = 0; j < size; j++) {
+////                    result[i][j] = Math.min(result[i][j], result[i][k] + result[k][j]);
+////                }
+////            }
+////        }
+////        return result;
+////    }
+////    public int solution(int[][] list) {
+////        int[] result = new int[list.length + 1];
+////
+////        for (int i = 0; i < list.length; i++) {
+////
+////            int next = i + (list[i][0]);
+////            if (next <= list.length) {
+////                result[next] = Math.max(list[i][1] + result[i], result[next]);
+////            }
+////
+////            result[i+1] = Math.max(result[i+1], result[i]);
+////        }
+////
+////        return result[list.length];
+////    }
+////    public int solution(int[][] list) {
+////        for (int i = 1; i < list.length; i++) {
+////            for (int j = 0; j < list[i].length; j++) {
+////                int max = 0;
+////                if (j < list[i - 1].length) {
+////                    max = Math.max(max, list[i - 1][j]);
+////                }
+////                if (j > 0) {
+////                    max = Math.max(max, list[i - 1][j - 1]);
+////                }
+////
+////                list[i][j] += max;
+////            }
+////        }
+////        int max = 0;
+////        for (int i = 0; i < list.length; i++) {
+////            max = Math.max(max, list[list.length - 1][i]);
+////        }
+////        return max;
+////    }
 //
-//            int next = i + (list[i][0]);
-//            if (next <= list.length) {
-//                result[next] = Math.max(list[i][1] + result[i], result[next]);
-//            }
-//
-//            result[i+1] = Math.max(result[i+1], result[i]);
-//        }
-//
-//        return result[list.length];
-//    }
-//    public int solution(int[][] list) {
-//        for (int i = 1; i < list.length; i++) {
-//            for (int j = 0; j < list[i].length; j++) {
-//                int max = 0;
-//                if (j < list[i - 1].length) {
-//                    max = Math.max(max, list[i - 1][j]);
-//                }
-//                if (j > 0) {
-//                    max = Math.max(max, list[i - 1][j - 1]);
-//                }
-//
-//                list[i][j] += max;
-//            }
-//        }
-//        int max = 0;
-//        for (int i = 0; i < list.length; i++) {
-//            max = Math.max(max, list[list.length - 1][i]);
-//        }
-//        return max;
-//    }
-
-//    public int solution(int[][] list) {
-//        for (int i = 1; i < list[0].length; i++) {
-//            for (int j = 0; j < list.length; j++) {
-//                int max = list[j][i - 1];
-//                if (j > 0) {
-//                    max = Math.max(max, list[j - 1][i - 1]);
-//                }
-//                if (j < list.length - 1) {
-//                    max = Math.max(max, list[j + 1][i - 1]);
-//                }
-//
-//                list[j][i] += max;
-//            }
-//        }
-//        int max = 0;
-//        for (int i = 0; i < list.length; i++) {
-//            max = Math.max(max, list[i][list[0].length - 1]);
-//        }
-//        return max;
-//    }
-}
+////    public int solution(int[][] list) {
+////        for (int i = 1; i < list[0].length; i++) {
+////            for (int j = 0; j < list.length; j++) {
+////                int max = list[j][i - 1];
+////                if (j > 0) {
+////                    max = Math.max(max, list[j - 1][i - 1]);
+////                }
+////                if (j < list.length - 1) {
+////                    max = Math.max(max, list[j + 1][i - 1]);
+////                }
+////
+////                list[j][i] += max;
+////            }
+////        }
+////        int max = 0;
+////        for (int i = 0; i < list.length; i++) {
+////            max = Math.max(max, list[i][list[0].length - 1]);
+////        }
+////        return max;
+////    }
+//}
---
+++ Jinhyuk/src/personal/CT1102P.java
@@ -0,0 +1,188 @@
+package personal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class CT1102P {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String str = br.readLine();
+//        StringTokenizer st = new StringTokenizer(str, " ");
+//        int size = Integer.valueOf(st.nextToken());
+//        int routeSize = Integer.valueOf(st.nextToken());
+//        int[][] list = new int[size][size];
+//        for (int i = 0; i < size; i++) {
+//            str = br.readLine();
+//            st = new StringTokenizer(str, " ");
+//            for (int j = 0; j < size; j++) {
+//                list[i][j] = Integer.valueOf(st.nextToken());
+//            }
+//        }
+//
+//        int[] route = new int[routeSize];
+//        str = br.readLine();
+//        st = new StringTokenizer(str, " ");
+//        for (int i = 0; i < routeSize; i++) {
+//            route[i] = Integer.valueOf(st.nextToken());
+//        }
+//        Solution temp = new Solution();
+//        System.out.println(temp.solution(list, route));
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String str = br.readLine();
+//        int airport = Integer.valueOf(str);
+//        int airplaneCount = Integer.valueOf(br.readLine());
+//        int[] targetAirport = new int[airplaneCount];
+//        for (int i = 0; i < airplaneCount; i++) {
+//            targetAirport[i] = Integer.valueOf(br.readLine());
+//        }
+//
+//        Solution temp = new Solution();
+//        System.out.println(temp.solution(targetAirport, airport));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int size = Integer.valueOf(st.nextToken());
+        int limit = Integer.valueOf(st.nextToken());
+
+        int[] list = new int[size];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < size; i++) {
+            list[i] = Integer.valueOf(st.nextToken());
+        }
+        Solution temp = new Solution();
+        System.out.println(temp.solution(list, limit));
+    }
+}
+
+class Solution {
+
+    public long solution(int[] list, long time) {
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (int i = 0; i < list.length; i++) {
+            int[] temp = new int[2];
+            temp[0] = list[i];
+            temp[1] = i + 1;
+            q.add(temp);
+        }
+
+        int minus = 0;
+        while ((q.peek()[0]- minus) * q.size() <= time) {
+            int[] temp = q.poll();
+            time -= (temp[0] - minus) * (q.size() + 1);
+            minus += (temp[0] - minus) * (q.size() + 1);
+        }
+
+        q = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
+
+        for (long i = 0; i < time; i++) {
+            int[] temp = q.poll();
+            if (temp[0] == 0) {
+                i--;
+                continue;
+            }
+            temp[0]--;
+            if (temp[0] > 0) q.add(temp);
+            if (q.size() == 0) return -1;
+        }
+        if (q.size() > 0) return q.poll()[1];
+        return -1;
+    }
+
+//    int solution(int[] list, int limit) {
+////        int count = 0 ;
+////        for (int i = 0; i < list.length; i++) {
+////            for (int j = i + 1; j < list.length; j++) {
+////                if(list[i] == list[j]) continue;
+////                count++;
+////            }
+////        }
+//        int count = 0;
+//        int[] countNum = new int[limit + 1];
+//        for (int i = 0; i < list.length; i++) {
+//            countNum[list[i]]++;
+//        }
+//
+//        int totalcount = list.length;
+//        for (int i = 1; i <= limit; i++) {
+//            totalcount -= countNum[i];
+//            count += countNum[i] * (totalcount);
+//        }
+//
+//        return count;
+//    }
+
+//    int findParent(int target, int[] parent) {
+//        if (target != parent[target]) {
+//            parent[target] = findParent(parent[target], parent);
+//        }
+//        return parent[target];
+//    }
+//
+//    void unionParent(int a, int b, int[] parent) {
+//        int aParent = findParent(a, parent);
+//        int bParent = findParent(b, parent);
+//        if (aParent < bParent) {
+//            parent[bParent] = aParent;
+//        } else {
+//            parent[aParent] = bParent;
+//        }
+//    }
+//
+//    public int solution(int[] array, int airport) {
+//        int[] parkParent = new int[airport + 1];
+//        for (int i = 0; i < parkParent.length; i++) {
+//            parkParent[i] = i;
+//        }
+//
+//        int count = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            int parentIdx = parkParent[array[i]];
+//            if (parentIdx == 0) return count;
+//            unionParent(array[i], parentIdx - 1, parkParent);
+//            count++;
+//        }
+//
+//        return count;
+//    }
+//    int findParent(int x, int[] parent) {
+//        if (x != parent[x]) {
+//            parent[x] = findParent(parent[x], parent);
+//        }
+//        return parent[x];
+//    }
+//
+//    void union(int a, int b, int[] parent) {
+//        int aParent = findParent(a, parent);
+//        int bParent = findParent(b, parent);
+//        if (aParent < bParent) {
+//            parent[bParent] = aParent;
+//        } else {
+//            parent[aParent] = bParent;
+//        }
+//    }
+//
+//    public String solution(int[][] list, int[] visitRoute) {
+//        int[] parent = new int[list.length];
+//        for (int i = 0; i < list.length; i++) {
+//            for (int j = 0; j < list.length; j++) {
+//                if (list[i][j] == 1) {
+//                    union(i, j, parent);
+//                }
+//            }
+//        }
+//
+//        int nowParent = 0;
+//        for (int i = 0; i < visitRoute.length; i++) {
+//            if (i > 0) {
+//                if (nowParent != findParent((visitRoute[i] - 1), parent)) return "NO";
+//            } else {
+//                nowParent = findParent((visitRoute[i] - 1), parent);
+//            }
+//        }
+//
+//        return "YES";
+//    }
+
+}
