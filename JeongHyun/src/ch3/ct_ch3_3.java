999a2c1221e32c4281894f305f9f8c98c9025904
OliviaYJH <0703olivia@naver.com>
2023-11-17 18:16:07 +0900

    이코테 3장

--- JeongHyun/src/ch3/ct_ch3_3.java
+++ JeongHyun/src/ch3/ct_ch3_3.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class ct_ch3_3 {
-    public static void main(String[] args) throws IOException {
+    /*public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -22,5 +22,24 @@ public class ct_ch3_3 {
         }
 
         System.out.println(max);
+    }*/
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int max = 0;
+
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++){
+            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            Arrays.sort(arr[i]);
+            max = Math.max(arr[i][0], max);
+        }
+
+        System.out.println(max);
+
     }
 }
--- JeongHyun/src/ch3/ct_ch3_4.java
+++ JeongHyun/src/ch3/ct_ch3_4.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class ct_ch3_4 {
-    public static void main(String[] args) throws IOException {
+    /*public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
@@ -21,5 +21,31 @@ public class ct_ch3_4 {
         }
 
         System.out.println(count);
+    }*/
+
+    public static void main(String[] args) throws IOException {
+        /*
+        N이 K로 나눠 떨어질 때만 나누기 가능
+        N에서 1 빼기
+        최소 횟수 구하기
+
+        26 5 -> 5 5 1
+        26 % 5 만큼 1을 뺌
+        나머지 만큼 나누기 가능
+         */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int result = n % k;
+        n -= result;
+        while (n != 1 ){
+            n /= k;
+            result++;
+        }
+        System.out.println(result);
     }
 }
