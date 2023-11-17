9aa39aea27892a2c3ad694d62872e4f65fc434ea
OliviaYJH <0703olivia@naver.com>
2023-11-17 22:25:45 +0900

    이코테 5장

--- JeongHyun/src/ch5/ct_ch5_3.java
+++ JeongHyun/src/ch5/ct_ch5_3.java
@@ -3,10 +3,11 @@ package ch5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class ct_ch5_3 { // dfs
-    static int[][] arr;
+    /*static int[][] arr;
     static int n, m;
 
     public static void main(String[] args) throws IOException {
@@ -47,6 +48,55 @@ public class ct_ch5_3 { // dfs
             // 상, 하, 좌, 우 위치들 재귀적으로 호출하기
             dfs(x+1, y);
             dfs(x-1, y);
+            dfs(x, y+1);
+            dfs(x, y-1);
+
+            return true;
+        }
+
+        return false;
+    }*/
+
+    static int n, m;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        /*
+        0: 뚫려 있는 부분
+        1: 칸막이
+        얼음 틀의 모양으로 총 아이스크림 개수 구하기
+        dfs -> true일 때 생성됨
+         */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(dfs(i, j))
+                    result++;
+            }
+        }
+        System.out.println(result);
+    }
+
+    static Boolean dfs(int x, int y) {
+        if (x < 0 || y < 0 || x >= n || y >= m) return false;
+
+        if (arr[x][y] == 0) {
+            arr[x][y] = 1; // 방문 처리
+
+            dfs(x + 1, y);
+            dfs(x - 1, y);
             dfs(x, y+1);
             dfs(x, y-1);
 
--- JeongHyun/src/ch5/ct_ch5_4.java
+++ JeongHyun/src/ch5/ct_ch5_4.java
@@ -9,7 +9,7 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class ct_ch5_4 { // bfs
-    static int[][] arr;
+    /*static int[][] arr;
     static int n, m;
     static int[] arr_x = {1, 0, -1, 0};
     static int[] arr_y = {0, 1, 0, -1};
@@ -57,11 +57,85 @@ public class ct_ch5_4 { // bfs
         }
 
         return arr[n-1][m-1];
+    }*/
+
+    static int n, m;
+    static int[][] arr;
+    static int[] arr_x = {1, 0, -1, 0};
+    static int[] arr_y = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        /*
+        동빈이는 (1,1) 미로는 (N, M)
+        한 번에 한 칸씩 이동 가능
+        0: 괴물 있는 곳, 1; 괴물 없는 곳
+        동빈이가 탈출 위해 움직여야 하는 최소 칸의 개수
+        칸은 시작 칸과 마지막 칸 모두 포함해 계산
+
+        bfs 사용
+        이동한 위치 방문처리하고 큐에 넣었다가 꺼내서 그에 인접한 위치들 추가하기
+         */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        // 입력 완료
+
+        // 호출
+        System.out.println(bfs(0, 0));; // 시작 위치
+
+    }
+
+    static int bfs(int x, int y) {
+        // 시작 지점에서 가까운 노드부터 차례대로 그래프의 모든 노드를 탐색
+        Queue<Maze_Node> node = new LinkedList<>();
+        node.add(new Maze_Node(x, y)); // 방문 처리
+
+        while (!node.isEmpty()) {
+            Maze_Node new_node = node.poll(); // 가장 최근거 꺼냄
+            x = new_node.x;
+            y = new_node.y;
+
+            // 4가지의 방향으로 확인하기
+            for (int i = 0; i < 4; i++) {
+                int nx = x + arr_x[i];
+                int ny = y + arr_y[i];
+
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue; // 공간을 벗어나면 무시
+
+                if (arr[nx][ny] == 0) continue; // 괴물 있으면 무시
+
+                if (arr[nx][ny] == 1) {
+                    // 괴물 없으면
+                    node.add(new Maze_Node(nx, ny)); // 큐에 추가
+                    arr[nx][ny] = arr[x][y] + 1; // 최단 거리 기록(얼만큼 갔는지 1씩 추가하면서 기록)
+                }
+            }
+        }
+
+        return arr[n-1][m-1];
     }
 
 }
 
-class Maze_Pair{
+class Maze_Node {
+    int x, y;
+
+    public Maze_Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+/*class Maze_Pair{
     int x, y;
 
     public Maze_Pair(int x, int y) {
@@ -70,4 +144,4 @@ class Maze_Pair{
     }
 
 
-}
+}*/
