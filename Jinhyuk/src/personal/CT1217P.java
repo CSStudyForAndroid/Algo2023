66e1d5681fcb2bd570f4d78a2331285e5a4cbfdd
jinee <sksowk156@gmail.com>
2023-12-17 13:17:54 +0900

    20231217, 알코테 ~주몽의 명령

--- Jinhyuk/src/personal/CT1217P.java
+++ Jinhyuk/src/personal/CT1217P.java
@@ -1,31 +1,55 @@
 package personal;
 
 import java.io.*;
+import java.util.*;
 
 public class CT1217P {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int startNum = 1;
-        int endNum = 1;
-        int sum = 1;
+//        int n = Integer.parseInt(br.readLine());
+//        int startNum = 1;
+//        int endNum = 1;
+//        int sum = 1;
+//        int count = 0;
+//        while (endNum <= n) {
+//            if (sum == n) {
+//                count++;
+//                endNum++;
+//                sum += endNum;
+//            } else if (sum < n) {
+//                endNum++;
+//                sum += endNum;
+//            } else {
+//                sum -= startNum;
+//                startNum++;
+//            }
+//        }
+        int source = Integer.valueOf(br.readLine());
+        int target = Integer.valueOf(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] list = new int[source];
+        for (int i = 0; i < source; i++) {
+            list[i] = Integer.valueOf(st.nextToken());
+        }
+        Arrays.sort(list);
+        int start = 0;
+        int end = source-1;
+        int sum = list[0];
         int count = 0;
-        while (endNum <= n) {
-            if (sum == n) {
+        while (start < end) {
+            if (list[start] + list[end] == target) {
+                start++;
+                end--;
                 count++;
-                endNum++;
-                sum += endNum;
-            } else if (sum < n) {
-                endNum++;
-                sum += endNum;
+            } else if (list[start] + list[end] < target) {
+                start++;
             } else {
-                sum -= startNum;
-                startNum++;
+                end--;
             }
         }
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(count);
-
+//        bw.write(count+"");
+        bw.write(count+"");
 
         bw.flush();
         bw.close();
